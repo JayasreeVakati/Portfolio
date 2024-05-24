bf1dc4b28411822e416232489b8e7365bc9b8b07
JayasreeVakati <VakatiJayasree93@gmail.com>
2024-05-24 20:49:14 +0530

    Create main.js

---
+++ root/main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".text", {
+  strings: ["Frontend Developer", "Programmer", "Web Developer"],
+  typeSpeed: 70,  // Corrected property name
+  backSpeed: 55,  // Corrected property name
+  loop: true
+});
